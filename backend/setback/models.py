48a18727f2cd870b688c56774899384313d4af32
Michael T. Mordowanec <michael.mordowanec@gmail.com>
2020-05-03 12:03:36 -0400

    Shuffle deck, deal, serve json

--- backend/setback/models.py
+++ backend/setback/models.py
@@ -1,5 +1,8 @@
+from random import shuffle
+
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.serializers import serialize
 
 
 class Player(models.Model):
@@ -11,21 +14,6 @@ class Player(models.Model):
         null=True,
     )
     display_name = models.CharField(max_length=30)
-
-
-class Game(models.Model):
-    """"""
-    players = models.ManyToManyField(
-        Player,
-        through='MatchPlayerGame',
-    )
-    team_one_score = models.IntegerField()
-    team_two_score = models.IntegerField()
-    next_to_play = models.ForeignKey(
-        'MatchPlayerGame',
-        on_delete=models.PROTECT,
-        related_name='+',
-    )
 
 
 class Card(models.Model):
@@ -53,6 +41,9 @@ class Card(models.Model):
     rank = models.IntegerField(choices=Rank.choices)
 
     def __unicode__(self):
+        return self.__str__()
+
+    def __str__(self):
         return f'{self.get_rank_display()} of {self.get_suit_display()}'
 
 
@@ -68,13 +59,106 @@ class MatchPlayerGame(models.Model):
         on_delete=models.PROTECT,
     )
     game = models.ForeignKey(
-        Game,
+        'Game',
         on_delete=models.CASCADE,
     )
     position = models.IntegerField(choices=Position.choices)
-    cards = models.ManyToManyField(Card, related_name='+')
+    hand = models.ManyToManyField(Card, related_name='+')
     currently_played = models.ForeignKey(
         Card,
         on_delete=models.DO_NOTHING,
         related_name='+',
+        blank=True,
+        null=True,
     )
+
+
+class Deck(models.Model):
+    """"""
+    position = models.IntegerField()
+    card = models.ForeignKey(Card, on_delete=models.PROTECT)
+    game = models.ForeignKey(
+        'Game',
+        on_delete=models.CASCADE,
+        related_name='+'
+    )
+
+
+class GameManager(models.Manager):
+    def new_game(self, players):
+        game = self.create(
+            team_one_score=0,
+            team_two_score=0,
+        )
+        game.add_players(players)
+        game.shuffle()
+        game.save()
+        return game
+
+
+class Game(models.Model):
+    """"""
+    players = models.ManyToManyField(
+        Player,
+        through='MatchPlayerGame',
+    )
+    team_one_score = models.IntegerField()
+    team_two_score = models.IntegerField()
+    next_to_play = models.ForeignKey(
+        'MatchPlayerGame',
+        on_delete=models.PROTECT,
+        related_name='+',
+        blank=True,
+        null=True,
+    )
+    deck = models.ManyToManyField(
+        Card,
+        through=Deck,
+    )
+
+    objects = GameManager()
+
+    def add_players(self, players):
+        for index, player in enumerate(players):
+            match_player_game = MatchPlayerGame.objects.create(
+                position=index,
+                game=self,
+                player=player,
+            )
+            if index == 0:
+                self.next_to_play = match_player_game
+
+    def shuffle(self):
+        self.deck.clear()
+        cards = list(Card.objects.all())
+        shuffle(cards)
+        for index, card in enumerate(cards):
+            deck = Deck.objects.create(
+                position=index,
+                game=self,
+                card=card,
+            )
+
+    def deal(self):
+        deck = self.deck.all()
+        for match_player_game in self.matchplayergame_set.all():
+            hand = deck[0:6]
+            match_player_game.hand.set(hand)
+            self.deck.remove(*hand)
+
+    def player_view(self, player):
+        hands = {}
+        for mpg in self.matchplayergame_set.all():
+            hands[mpg.player.display_name] = []
+            for card in mpg.hand.all():
+                if mpg.player == player:
+                    hands[mpg.player.display_name].append(
+                        {
+                            'id': card.id,
+                            'rank': card.rank,
+                            'suit': card.suit,
+                        }
+                    )
+                else:
+                    hands[mpg.player.display_name].append({})
+        return hands
--- backend/setback/views.py
+++ backend/setback/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+from .models import (
+    Player,
+    Game,
+)
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world.")
+    players = []
+    for i in range(4):
+        player, _ = Player.objects.get_or_create(
+            display_name=f'Player ${i}',
+        )
+        players.append(player)
+#   game = Game.objects.new_game(players)
+
+    game = Game.objects.get(pk=4)
+    game.shuffle()
+    game.deal()
+
+    return JsonResponse({
+        'data': game.player_view(Player.objects.get(pk=2)),
+    })
